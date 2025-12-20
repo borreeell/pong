17fc02faf089deaa845f4b8bd294cf931eae5538
Pablo Abad <pablo.abad.girona2019@gmail.com>
2025-12-20 13:36:40 +0100

    Update pablo.h

--- pong/pablo.h
+++ pong/pablo.h
@@ -1,22 +1,23 @@
-#pragma once // Evita duplicats
+#pragma once 
 
 #include <SFML/Graphics.hpp> // Llibreria gràfica
 
 // Estructura per a les pales
-struct Pala { // Defineix pala
+struct Pala // Defineix pala
+{ 
     float x, y; // Posició actual
     float amplada, alcada; // Mides pala
     int puntuacio; // Punts jugador
 };
 
-// Inicialitzar la pala
+// INICIAR LA PALA
 void inicialitzarPala(Pala& pala, float x, float y); // Valors inicials
 
-// Moure la pala del jugador
+// MOURE PALA JUGADOR
 void mourePala(Pala& pala, float tempsTranscorregut, bool amunt, bool avall); // Moviment jugador
 
-// IA simple per a la pala de l'ordinador
+// MOURE PALA AMB RANDOM
 void moureIA(Pala& pala, float pilotaX, float pilotaY, float tempsTranscorregut); // Moviment IA
 
-// Dibuixar la pala
+// DIBUIX DE LA PALA
 void dibuixarPala(sf::RenderWindow& finestra, const Pala& pala); // Renderitza pala
