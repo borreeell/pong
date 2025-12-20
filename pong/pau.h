9692ea3e6409c668a538e4bc3f3e57b9a6933368
Pablo Abad <pablo.abad.girona2019@gmail.com>
2025-12-20 13:39:47 +0100

    Update pau.h

--- pong/pau.h
+++ pong/pau.h
@@ -1,16 +1,17 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp> // Llibreria gràfica SFML
 
-#include "pablo.h"
+#include "pablo.h" // Inclou definició pales
 
 // Estructura que representa la pilota
-struct Pilota {
-    float x, y;
-    float radi;
-    float velocitatX, velocitatY;
+struct Pilota // Defineix la pilota
+{ 
+    float x, y; // Posició de pilota
+    float radi; // Mida del cercle
+    float velocitatX, velocitatY; // Vectors de moviment
 };
 
-void inicialitzarPilota(Pilota& pilota);
-void actualitzarPilota(Pilota& pilota, float tempsTranscorregut);
-void gestionarCollisions(Pilota& pilota, const Pala& palaEsquerra, const Pala& palaDreta);
-bool comprovarPuntuacio(const Pilota& pilota, Pala& palaEsquerra, Pala& palaDreta);
-void dibuixarPilota(sf::RenderWindow& finestra, const Pilota& pilota);
+void inicialitzarPilota(Pilota& pilota); // Configura valors inicials
+void actualitzarPilota(Pilota& pilota, float tempsTranscorregut); // Mou la pilota
+void gestionarCollisions(Pilota& pilota, const Pala& palaEsquerra, const Pala& palaDreta); // Gestiona xocs pales
+bool comprovarPuntuacio(const Pilota& pilota, Pala& palaEsquerra, Pala& palaDreta); // Detecta punt marcat
+void dibuixarPilota(sf::RenderWindow& finestra, const Pilota& pilota); // Mostra pilota finestra
