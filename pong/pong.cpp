fca09f811995360d54258e498490ca3410363665
Pablo Abad <pablo.abad.girona2019@gmail.com>
2025-12-20 13:43:45 +0100

    Update pong.cpp

--- pong/pong.cpp
+++ pong/pong.cpp
@@ -1,127 +1,112 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <string>
+#include <iostream> // Entrada i sortida
+#include <cstdlib> // Utilitats estàndard
+#include <ctime> // Control de temps
+#include <string> // Gestió de textos
 
-#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp> // Motor gràfic SFML
 
-#include "pablo.h"
-#include "pau.h"
+#include "pablo.h" // Capçalera pales
+#include "pau.h" // Capçalera pilota
 
-using namespace std;
-using namespace sf; 
-
+using namespace std; // Estil estàndard
+using namespace sf; // Estil SFML
 
 // Constants del joc
-const int AMPLADA_FINESTRA = 800;
-const int ALCADA_FINESTRA = 600;
-const float AMPLADA_PALA = 15.0f;
-const float ALCADA_PALA = 100.0f;
+const int AMPLADA_FINESTRA = 800; // Ample pantalla
+const int ALCADA_FINESTRA = 600; // Alt pantalla
+const float AMPLADA_PALA = 15.0f; // Ample rectangle
+const float ALCADA_PALA = 100.0f; // Alt rectangle
 
-
-void dibuixarLiniaCentral(RenderWindow& finestra) {
-    for (int i = 0; i < ALCADA_FINESTRA; i += 20) {
-        RectangleShape linia(Vector2f(2, 10));
-        linia.setPosition({ AMPLADA_FINESTRA / 2.0f, static_cast<float>(i) }); // Dibuixa la linia al mitg de la finestra
-        linia.setFillColor(Color(100, 100, 100));
-        finestra.draw(linia);
+void dibuixarLiniaCentral(RenderWindow& finestra) // Dibuixa camp
+{ 
+    for (int i = 0; i < ALCADA_FINESTRA; i += 20) // Bucle línies
+    { 
+        RectangleShape linia(Vector2f(2, 10)); // Crea segment
+        linia.setPosition({ AMPLADA_FINESTRA / 2.0f, static_cast<float>(i) }); // Centra segment
+        linia.setFillColor(Color(100, 100, 100)); // Color gris
+        finestra.draw(linia); // Renderitza segment
     }
 }
 
-void dibuixarPuntuacio(RenderWindow& finestra, const Pala& palaEsquerra, const Pala& palaDreta, Font& font) {
-    Text puntuacioEsquerra(font);
-    puntuacioEsquerra.setString(std::to_string(palaEsquerra.puntuacio)); // Converteix la puntuacio de int a string
-    puntuacioEsquerra.setCharacterSize(50);
-    puntuacioEsquerra.setFillColor(sf::Color::White);
-    puntuacioEsquerra.setPosition({ AMPLADA_FINESTRA / 4.0f, 20 }); // (x: 200, y: 20)
+void dibuixarPuntuacio(RenderWindow& finestra, const Pala& palaEsquerra, const Pala& palaDreta, Font& font) // Mostra marcadors
+{ 
+    Text puntuacioEsquerra(font); // Text esquerre
+    puntuacioEsquerra.setString(std::to_string(palaEsquerra.puntuacio)); // Text a string
+    puntuacioEsquerra.setCharacterSize(50); // Mida lletra
+    puntuacioEsquerra.setFillColor(sf::Color::White); // Color blanc
+    puntuacioEsquerra.setPosition({ AMPLADA_FINESTRA / 4.0f, 20 }); // Posició esquerra
 
-    Text puntuacioDreta(font);
-    puntuacioDreta.setFont(font);
-    puntuacioDreta.setString(std::to_string(palaDreta.puntuacio));
-    puntuacioDreta.setCharacterSize(50);
-    puntuacioDreta.setFillColor(sf::Color::White);
-    puntuacioDreta.setPosition({ 3 * AMPLADA_FINESTRA / 4.0f, 20 }); // (x: 600, y: 20)
+    Text puntuacioDreta(font); // Text dret
+    puntuacioDreta.setFont(font); // Assigna font
+    puntuacioDreta.setString(std::to_string(palaDreta.puntuacio)); // Valor marcador
+    puntuacioDreta.setCharacterSize(50); // Mida lletra
+    puntuacioDreta.setFillColor(sf::Color::White); // Color blanc
+    puntuacioDreta.setPosition({ 3 * AMPLADA_FINESTRA / 4.0f, 20 }); // Posició dreta
 
-    finestra.draw(puntuacioEsquerra);
-    finestra.draw(puntuacioDreta);
+    finestra.draw(puntuacioEsquerra); // Dibuixa esquerre
+    finestra.draw(puntuacioDreta); // Dibuixa dret
 }
 
-int main() {
-    srand(static_cast<unsigned>(time(0)));
+int main() // Funció principal
+{ 
+    srand(static_cast<unsigned>(time(0))); // Llavor aleatòria
 
-    // Crear finestra
-    VideoMode vm({ 800, 600 });
-    RenderWindow window(vm, "Pong");
+    VideoMode vm({ 800, 600 }); // Mode vídeo
+    RenderWindow window(vm, "Pong"); // Crea finestra
 
-    // Carregar font
-    Font font("KOMIKAP_.ttf");
+    Font font("KOMIKAP_.ttf"); // Carrega tipografia
 
-    // Inicialitzar elements del joc
-    Pala palaEsquerra, palaDreta;
-    Pilota pilota;
+    Pala palaEsquerra, palaDreta; // Declara pales
+    Pilota pilota; // Declara pilota
 
-    inicialitzarPala(palaEsquerra, 30, ALCADA_FINESTRA / 2 - ALCADA_PALA / 2);
-    inicialitzarPala(palaDreta, AMPLADA_FINESTRA - 30 - AMPLADA_PALA, ALCADA_FINESTRA / 2 - ALCADA_PALA / 2);
-    inicialitzarPilota(pilota);
+    inicialitzarPala(palaEsquerra, 30, ALCADA_FINESTRA / 2 - ALCADA_PALA / 2); // Inicia esquerra
+    inicialitzarPala(palaDreta, AMPLADA_FINESTRA - 30 - AMPLADA_PALA, ALCADA_FINESTRA / 2 - ALCADA_PALA / 2); // Inicia dreta
+    inicialitzarPilota(pilota); // Inicia pilota
 
-    Clock rellotge;
+    Clock rellotge; // Control fotogrames
 
-    // Input del jugador
-    bool amuntPremut = false;
-    bool avallPremut = false;
+    bool amuntPremut = false; // Estat tecla W
+    bool avallPremut = false; // Estat tecla S
 
-    // Bucle principal
-    while (window.isOpen()) {
-        float tempsTranscorregut = rellotge.restart().asSeconds();
+    while (window.isOpen()) // Bucle joc
+    { 
+        float tempsTranscorregut = rellotge.restart().asSeconds(); // Delta time
 
-        // Gestionar events
-        while (const optional event = window.pollEvent()) {
-            if (event->is<Event::Closed>()) window.close();
+        while (const optional event = window.pollEvent()) { // Gestió events
+            if (event->is<Event::Closed>()) window.close(); // Tanca finestra
 
-            // Mou la pala amunt o avall segons la tecla que s'apreti
-            if (const auto* key = event->getIf<Event::KeyPressed>()) {
-                if (key->scancode == Keyboard::Scancode::W) {
-                    amuntPremut = true;
-                }
-
-                if (key->scancode == Keyboard::Scancode::S) {
-                    avallPremut = true;
-                }
+            if (const auto* key = event->getIf<Event::KeyPressed>()) // Tecla premuda
+            {
+                if (key->scancode == Keyboard::Scancode::W) amuntPremut = true; // Activa amunt
+                if (key->scancode == Keyboard::Scancode::S) avallPremut = true; // Activa avall
             }
 
-            // Deixa de moure la pala al deixar anar la tecla
-            if (const auto* key = event->getIf<Event::KeyReleased>()) {
-                if (key->scancode == Keyboard::Scancode::W) {
-                    amuntPremut = false;
-                }
-
-                if (key->scancode == Keyboard::Scancode::S) {
-                    avallPremut = false;
-                }
+            if (const auto* key = event->getIf<Event::KeyReleased>()) // Tecla soltada
+            { 
+                if (key->scancode == Keyboard::Scancode::W) amuntPremut = false; // Atura amunt
+                if (key->scancode == Keyboard::Scancode::S) avallPremut = false; // Atura avall
             }
         }
 
-        // Actualitzar
-        mourePala(palaEsquerra, tempsTranscorregut, amuntPremut, avallPremut);
-        moureIA(palaDreta, pilota.x, pilota.y, tempsTranscorregut);
-        actualitzarPilota(pilota, tempsTranscorregut);
-        gestionarCollisions(pilota, palaEsquerra, palaDreta);
+        mourePala(palaEsquerra, tempsTranscorregut, amuntPremut, avallPremut); // Actualitza jugador
+        moureIA(palaDreta, pilota.x, pilota.y, tempsTranscorregut); // Actualitza IA
+        actualitzarPilota(pilota, tempsTranscorregut); // Actualitza pilota
+        gestionarCollisions(pilota, palaEsquerra, palaDreta); // Comprova xocs
 
-        // Comprovar si s'ha marcat
-        if (comprovarPuntuacio(pilota, palaEsquerra, palaDreta)) {
-            inicialitzarPilota(pilota);
+        if (comprovarPuntuacio(pilota, palaEsquerra, palaDreta)) // Si gol
+        {
+            inicialitzarPilota(pilota); // Reinicia pilota
         }
 
-        // Dibuixar els elements
-        window.clear(Color::Black);
-        dibuixarLiniaCentral(window);
-        dibuixarPala(window, palaEsquerra);
-        dibuixarPala(window, palaDreta);
-        dibuixarPilota(window, pilota);
-        dibuixarPuntuacio(window, palaDreta, palaEsquerra, font);
- 
-        window.display();
+        window.clear(Color::Black); // Neteja fons
+        dibuixarLiniaCentral(window); // Dibuixa camp
+        dibuixarPala(window, palaEsquerra); // Dibuixa esquerra
+        dibuixarPala(window, palaDreta); // Dibuixa dreta
+        dibuixarPilota(window, pilota); // Dibuixa pilota
+        dibuixarPuntuacio(window, palaDreta, palaEsquerra, font); // Dibuixa punts
+
+        window.display(); // Mostra canvis
     }
 
-    return 0;
+    return 0; // Finalitza programa
 }
